e9608e232185a8d27e86d08de320884b67fdbe4f
Ftgeorge <ftgeorgean@gmail.com>
2023-01-26 17:09:16 +0100

    Calling from the Hive db

---
+++ lib/data/database.dart
@@ -0,0 +1,23 @@
+class ToDoDataBase {
+  List ToDoList = [];
+  //reference the store
+  final _myBox = Hive.box('mybox');
+
+  //If this is the 1st time ever opening this app
+  void createInitialData() {
+    ToDoList = [
+      ["Make Tutorial", false],
+      ["Do Exercise", false],
+    ];
+  }
+
+  //load
+  void loadData() {
+    ToDoList = _myBox.get("TODOLIST");
+  }
+
+  //upload
+  void updateDatabase() {
+    _myBox.put("TODOLIST", ToDoList);
+  }
+}
